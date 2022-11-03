638a19bff79f6f3be8c390c71673caa2a1644983
CalvinDT <ct223344@gmail.com>
2022-11-02 22:28:09 -0400

    Updated file layout

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 // TODO: Include packages needed for this application
-const fs = require("fs")
-const inquirer = require("inquirer")
-const path = require("path")
-const generateMarkdown = require("./utils/generateMarkdown")
+const inq = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
 const questions = [
